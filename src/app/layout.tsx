0c3a6942aa5fc48156645e9731feac77f200b11c
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-05-23 15:43:08 +0300

    feat: Add Apple touch icon, favicon, and manifest file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,33 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* This icon is used when the website is saved to the home screen of an Apple device. */}
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/assets/icons/apple-touch-icon.png"
+        />
+
+        {/* Favicon is a small icon that is displayed in the browser's tab or bookmark bar. */}
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/assets/icons/favicon-32x32.png"
+        />
+
+        {/* This is a smaller version of the favicon used in certain contexts. */}
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/assets/icons/favicon-16x16.png"
+        />
+
+        {/* Links manifest file that provides information about the web application, such as its name, icons, and other properties in PWAs. */}
+        <link rel="manifest" href="/assets/icons/site.webmanifest" />
+      </head>
       <body className={inter.className}>
         <Providers>{children}</Providers>
       </body>
