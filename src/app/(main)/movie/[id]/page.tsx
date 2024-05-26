912c9977e44d77b151f5a0ad90ab373087d84fd7
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-05-26 09:29:17 +0300

    feat: Add MovieDetailsPage  for displaying a single movie details

---
+++ src/app/(main)/movie/[id]/page.tsx
@@ -0,0 +1,52 @@
+import MovieDetailsPage from "@/components/page/MovieDetailsPage";
+import { MediaUrl, MoviesUrl } from "@/lib/constants";
+import {
+  Collection,
+  TMediaCreditsResponse,
+  TMediaResponse,
+  TMovie,
+  TMovieDetailsResponse,
+} from "@/lib/types";
+import { getMedia } from "@/lib/utils";
+import { notFound } from "next/navigation";
+import React from "react";
+
+const MovieDetailsFetchPage = async ({
+  params: { id },
+}: {
+  params: { id: string };
+}) => {
+  const movie = (await getMedia(
+    MoviesUrl.Origin + id.toString() + "?language=en-US"
+  )) as TMovieDetailsResponse;
+
+  if (!movie?.overview) notFound();
+
+  const credits = (await getMedia(
+    MoviesUrl.Origin + id + "/credits?language=en-US"
+  )) as TMediaCreditsResponse;
+
+  const { results: similarMovies } = (await getMedia(
+    MoviesUrl.Origin + id + "/similar?language=en-US&page=1"
+  )) as TMediaResponse<TMovie>;
+
+  const { results: recommendedMovies } = (await getMedia(
+    MoviesUrl.Origin + id + "/recommendations?language=en-US&page=1"
+  )) as TMediaResponse<TMovie>;
+  
+  const collection = movie.belongs_to_collection && (await getMedia(
+    MediaUrl +"collection/" + movie.belongs_to_collection.id + "?language=en-US"
+  )) as Collection;
+
+  return (
+    <MovieDetailsPage
+      movie={movie}
+      credits={credits}
+      recommendedMovies={recommendedMovies}
+      similarMovies={similarMovies}
+      collection={collection || {}}
+    />
+  );
+};
+
+export default MovieDetailsFetchPage;
---
+++ src/components/page/MovieDetailsPage.tsx
@@ -0,0 +1,140 @@
+import React from "react";
+import MediaCard from "@/components/ui/MediaCard";
+import MediaCategory from "@/components/ui/MediaCategory";
+import MediaCategorySlider from "@/components/ui/sliders/MediaCategorySlider";
+import DetailsPosterContent from "@/components/ui/hero/DetailsPosterContent";
+import PosterContainer from "@/components/ui/hero/PosterContainer";
+import { ImageSize, MediaType } from "@/lib/constants";
+import {
+  Collection,
+  TMediaCreditsResponse,
+  TMovie,
+  TMovieDetailsResponse,
+} from "@/lib/types";
+import { getImageUrl } from "@/lib/utils";
+
+const MovieDetailsPage = ({
+  movie,
+  credits,
+  similarMovies,
+  recommendedMovies,
+  collection,
+}: {
+  movie: TMovieDetailsResponse;
+  credits: TMediaCreditsResponse;
+  similarMovies: TMovie[];
+  recommendedMovies: TMovie[];
+  collection: Collection;
+}) => {
+  const { cast, crew } = credits;
+  const img = getImageUrl(collection.backdrop_path, ImageSize.Large);
+
+  return (
+    <>
+      <PosterContainer media={movie}>
+        <DetailsPosterContent
+          type={MediaType.MovieDetails}
+          media={movie}
+          credits={credits}
+        />
+      </PosterContainer>
+
+      {collection.id && (
+        <div
+          style={{
+            backgroundImage: `url(${img})`,
+            backgroundAttachment: "fixed",
+            backgroundPosition: "50% 20%",
+          }}
+          className={`px-3 py-7 md:px-10 md:py-16 bg-center bg-no-repeat bg-cover relative`}
+        >
+          <div className="absolute inset-0 bg-black bg-opacity-60" />
+          <div className="relative z-10">
+            <MediaCategory media={collection.parts} heading={collection.name}>
+              {collection.overview && (
+                <p className="mb-14 -mt-3 md:-mt-6 px-5 md:px-20 text-gray-300">
+                  {collection.overview}
+                </p>
+              )}
+              <MediaCategorySlider mediaLength={collection.parts.length}>
+                {collection.parts.map((movie) => (
+                  <MediaCard
+                    key={movie.id}
+                    media={movie}
+                    path={`/movie/${movie.id}`}
+                  />
+                ))}
+              </MediaCategorySlider>
+            </MediaCategory>
+          </div>
+        </div>
+      )}
+
+      <MediaCategory
+        media={cast}
+        path={`/movie/${movie.id}/people/cast`}
+        heading="Cast"
+      >
+        <MediaCategorySlider mediaLength={cast.length}>
+          {cast.map((castMember) => (
+            <MediaCard
+              key={castMember.id}
+              media={castMember}
+              path={`/people/${castMember.id}`}
+            />
+          ))}
+        </MediaCategorySlider>
+      </MediaCategory>
+
+      <MediaCategory
+        media={crew}
+        path={`/movie/${movie.id}/people/crew`}
+        heading="Crew"
+      >
+        <MediaCategorySlider mediaLength={crew.length}>
+          {crew.map((crewMember) => (
+            <MediaCard
+              key={crewMember.id}
+              media={crewMember}
+              path={`/people/${crewMember.id}`}
+            />
+          ))}
+        </MediaCategorySlider>
+      </MediaCategory>
+
+      <MediaCategory
+        media={recommendedMovies}
+        path={`/movie/${movie.id}/recommendations`}
+        heading="Recommended"
+      >
+        <MediaCategorySlider mediaLength={recommendedMovies.length}>
+          {recommendedMovies.map((recommendedMovie) => (
+            <MediaCard
+              key={recommendedMovie.id}
+              media={recommendedMovie}
+              path={`/movie/${recommendedMovie.id}`}
+            />
+          ))}
+        </MediaCategorySlider>
+      </MediaCategory>
+
+      <MediaCategory
+        media={similarMovies}
+        path={`/movie/${movie.id}/similar`}
+        heading="Similar"
+      >
+        <MediaCategorySlider mediaLength={similarMovies.length}>
+          {similarMovies.map((similarMovie) => (
+            <MediaCard
+              key={similarMovie.id}
+              media={similarMovie}
+              path={`/movie/${similarMovie.id}`}
+            />
+          ))}
+        </MediaCategorySlider>
+      </MediaCategory>
+    </>
+  );
+};
+
+export default MovieDetailsPage;
