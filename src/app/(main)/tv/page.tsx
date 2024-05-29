3e88caf80d4b7da5abf48bb441896e0f233556e4
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-05-29 23:55:19 +0300

    feat: Add TVShowsPageDataFetch component for fetching TV show data

---
+++ src/app/(main)/tv/page.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { getMedia } from "@/lib/utils";
+import { TVShowsUrl } from "@/lib/constants";
+import { TMediaResponse, TTVShow } from "@/lib/types";
+import TVShowsPage from "@/components/page/TVShowsPage";
+
+const TVShowsPageDataFetch = async () => {
+  const { results: popularTVShows } = (await getMedia(
+    TVShowsUrl.Popular + "?language=en-US&page=1"
+  )) as TMediaResponse<TTVShow>;
+  const { results: trendingDailyTVShows } = (await getMedia(
+    TVShowsUrl.Trending + "/day?language=en-US"
+  )) as TMediaResponse<TTVShow>;
+  const { results: upcomingTVShows } = (await getMedia(
+    TVShowsUrl.Upcoming + "?language=en-US&page=1"
+  )) as TMediaResponse<TTVShow>;
+  const { results: topRatedTVShows } = (await getMedia(
+    TVShowsUrl.TopRated + "?language=en-US&page=1"
+  )) as TMediaResponse<TTVShow>;
+
+  return (
+    <>
+      <TVShowsPage
+        popularTVShows={popularTVShows}
+        trendingDailyTVShows={trendingDailyTVShows}
+        topRatedTVShows={topRatedTVShows}
+        upcomingTVShows={upcomingTVShows}
+      />
+    </>
+  );
+};
+
+export default TVShowsPageDataFetch;
