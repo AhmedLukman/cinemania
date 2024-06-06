44fadc288c6a5b55554a03dfd635127babed0ae5
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-06-07 00:03:13 +0300

    refactor: Update MediaCard to accept h2 element as prop

--- src/components/ui/MediaGrid.tsx
+++ src/components/ui/MediaGrid.tsx
@@ -1,18 +1,13 @@
 import React from "react";
 import MediaCard from "./MediaCard";
-import { TMediaGrid} from "@/lib/types";
+import { TMediaGrid } from "@/lib/types";
 
-const MediaGrid = ({
-  media,
-  path
-}: {
-  media: TMediaGrid;
-  path: string
-}) => {
+const MediaGrid = ({ media, path }: { media: TMediaGrid; path: string }) => {
   return (
     <div className="grid grid-cols-2 md:grid-cols-4 gap-y-8">
       {media?.map((media) => (
         <MediaCard
+          element="h2"
           key={media.id}
           media={media}
           path={`/${path}/${media.id}`}
