a40ed1d8dd5b6ddfec604b46f94225b003cff640
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-06-07 11:54:40 +0300

    feat: Add Github as a provider for NextAuth

--- src/auth.ts
+++ src/auth.ts
@@ -2,11 +2,12 @@ import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "./lib/prisma";
 import Google from "next-auth/providers/google";
+import Github from "next-auth/providers/github";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   theme: {
     logo: "/assets/icons/cinemania-logo.png",
   },
   adapter: PrismaAdapter(prisma),
-  providers: [Google],
+  providers: [Google, Github],
 });
