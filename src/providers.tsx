b4b915a2abf0645c9656c20550d94e92ce95b9fe
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-06-07 20:10:27 +0300

    chore: Add next auth SessionProvider to Providers component

--- src/providers.tsx
+++ src/providers.tsx
@@ -2,8 +2,13 @@
 
 import { NextUIProvider } from "@nextui-org/react";
 import { useRouter } from "next/navigation";
+import { SessionProvider } from "next-auth/react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const router = useRouter();
-  return <NextUIProvider navigate={router.push}>{children}</NextUIProvider>;
+  return (
+    <SessionProvider>
+      <NextUIProvider navigate={router.push}>{children}</NextUIProvider>
+    </SessionProvider>
+  );
 }
