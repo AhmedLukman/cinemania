b3b1d90a46673a915a9610855873afa9bd31b456
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-06-07 11:17:59 +0300

    chore: Connect prisma together with next auth v5, and migrate

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,15 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { Pool } from "@neondatabase/serverless";
+
+// Prevents multiple instances of Prisma Client in development and make prisma client edge compatible
 
 const prismaClientSingleton = () => {
-  return new PrismaClient();
+  const neon = new Pool({
+    connectionString: process.env.POSTGRES_PRISMA_URL,
+  });
+  const adapter = new PrismaNeon(neon);
+  return new PrismaClient({ adapter } as any);
 };
 
 declare const globalThis: {
