a09b0c0af17dcdf1f1f6ae2ae25462fc9d2662e4
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-06-09 14:14:13 +0300

    feat: Add TVShowDetailsPage component

---
+++ src/components/page/TVShowDetailsPage.tsx
@@ -0,0 +1,140 @@
+import React from "react";
+import MediaCard from "@/components/ui/MediaCard";
+import MediaCategory from "@/components/ui/MediaCategory";
+import MediaCategorySlider from "@/components/ui/sliders/MediaCategorySlider";
+import DetailsPosterContent from "@/components/ui/hero/DetailsPosterContent";
+import PosterContainer from "@/components/ui/hero/PosterContainer";
+import { ImageSize, MediaType } from "@/lib/constants";
+import {
+  TMediaCreditsResponse,
+  TTVShow,
+  TTVShowDetailsResponse,
+} from "@/lib/types";
+import Image from "next/image";
+import { dataUrl, getImageUrl } from "@/lib/utils";
+import { PlaceholderValue } from "next/dist/shared/lib/get-img-props";
+
+const TVShowDetailsPage = ({
+  tvShow,
+  credits,
+  similarTVShows,
+  recommendedTVShows,
+}: {
+  tvShow: TTVShowDetailsResponse;
+  credits: TMediaCreditsResponse;
+  similarTVShows: TTVShow[];
+  recommendedTVShows: TTVShow[];
+}) => {
+  const { cast, crew } = credits;
+  const { networks, seasons } = tvShow;
+  return (
+    <>
+      <PosterContainer media={tvShow}>
+        <DetailsPosterContent
+          type={MediaType.TVDetails}
+          media={tvShow}
+          credits={credits}
+        />
+      </PosterContainer>
+
+      <MediaCategory media={networks} heading="Network">
+        <MediaCategorySlider mediaLength={networks.length}>
+          {networks.map((network) => (
+            <div
+              className="px-2 md:px-4 flex h-20 justify-center items-center"
+              key={network.id}
+            >
+              <Image
+                unoptimized
+                placeholder={dataUrl as PlaceholderValue}
+                className="w-full h-full object-contain"
+                width={300}
+                height={10}
+                alt={network.name + " logo"}
+                src={getImageUrl(network.logo_path, ImageSize.Small)}
+              />
+            </div>
+          ))}
+        </MediaCategorySlider>
+      </MediaCategory>
+
+      <MediaCategory media={seasons} heading="Seasons">
+        <MediaCategorySlider mediaLength={seasons.length}>
+          {seasons.map((season) => (
+            <MediaCard
+              key={season.id}
+              media={season}
+              path={`/tv/${tvShow.id}/season/${season.season_number}`}
+            />
+          ))}
+        </MediaCategorySlider>
+      </MediaCategory>
+
+      <MediaCategory
+        media={cast}
+        path={`/tv/${tvShow.id}/people/cast`}
+        heading="Cast"
+      >
+        <MediaCategorySlider mediaLength={cast.length}>
+          {cast.map((castMember) => (
+            <MediaCard
+              key={castMember.id}
+              media={castMember}
+              path={`/people/${castMember.id}`}
+            />
+          ))}
+        </MediaCategorySlider>
+      </MediaCategory>
+
+      <MediaCategory
+        media={crew}
+        path={`/tv/${tvShow.id}/people/crew`}
+        heading="Crew"
+      >
+        <MediaCategorySlider mediaLength={crew.length}>
+          {crew.map((crewMember) => (
+            <MediaCard
+              key={crewMember.id}
+              media={crewMember}
+              path={`/people/${crewMember.id}`}
+            />
+          ))}
+        </MediaCategorySlider>
+      </MediaCategory>
+
+      <MediaCategory
+        media={recommendedTVShows}
+        path={`/tv/${tvShow.id}/recommendations`}
+        heading="Recommended"
+      >
+        <MediaCategorySlider mediaLength={recommendedTVShows.length}>
+          {recommendedTVShows.map((recommendedTVShow) => (
+            <MediaCard
+              key={recommendedTVShow.id}
+              media={recommendedTVShow}
+              path={`/tv/${recommendedTVShow.id}`}
+            />
+          ))}
+        </MediaCategorySlider>
+      </MediaCategory>
+
+      <MediaCategory
+        media={similarTVShows}
+        path={`/tv/${tvShow.id}/similar`}
+        heading="Similar"
+      >
+        <MediaCategorySlider mediaLength={similarTVShows.length}>
+          {similarTVShows.map((similarTVShow) => (
+            <MediaCard
+              key={similarTVShow.id}
+              media={similarTVShow}
+              path={`/tv/${similarTVShow.id}`}
+            />
+          ))}
+        </MediaCategorySlider>
+      </MediaCategory>
+    </>
+  );
+};
+
+export default TVShowDetailsPage;
