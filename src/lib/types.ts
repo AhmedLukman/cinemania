5806b5c398266b210a051e303d563aca9674143c
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-06-10 23:21:20 +0300

    chore: Add PersonLink type definition

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -430,3 +430,17 @@ export type TMediaCredits =
 export type TMediaActions =
   | { type: PosterType.TV; media: TTVShow }
   | { type: PosterType.Movie; media: TMovie };
+
+export type PersonLink = {
+  id: number;
+  freebase_mid: string;
+  freebase_id: string;
+  imdb_id: string;
+  tvrage_id: number;
+  wikidata_id: string;
+  facebook_id: string;
+  instagram_id: string;
+  tiktok_id: string;
+  twitter_id: string;
+  youtube_id: string;
+};
