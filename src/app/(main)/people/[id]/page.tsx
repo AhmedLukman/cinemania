d4672e515a13ede25ef4fcb04aa43a562cc53dff
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-06-12 01:23:18 +0300

    feat: Implement dynamic fetching of socials links

--- src/app/(main)/people/[id]/page.tsx
+++ src/app/(main)/people/[id]/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { MediaUrl } from "@/lib/constants";
 import {
+  PersonLink,
   TMovie,
   TPersonDetails,
   TPersonMediaCredits,
@@ -25,12 +26,17 @@ const PeopleDetailsFetchPage = async ({
   const personTVShowCredits = (await getMedia(
     `${MediaUrl}person/${id}/tv_credits`
   )) as TPersonMediaCredits<TTVShow>;
-  
+
+  const personLink = (await getMedia(
+    `${MediaUrl}person/${id}/external_ids`
+  )) as PersonLink;
+
   return (
     <PeopleDetailsPage
       personDetails={personDetails}
       personMovieCredits={personMovieCredits}
       personTVShowCredits={personTVShowCredits}
+      personLink={personLink}
     />
   );
 };
--- src/components/page/PeopleDetailsPage.tsx
+++ src/components/page/PeopleDetailsPage.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {
+  PersonLink,
   TMovie,
   TPersonDetails,
   TPersonMediaCredits,
@@ -12,22 +13,18 @@ const PeopleDetailsPage = ({
   personDetails,
   personMovieCredits: { cast: moviesCast, crew: moviesCrew },
   personTVShowCredits: { cast: tvShowsCast, crew: tvShowsCrew },
+  personLink
 }: {
   personDetails: TPersonDetails;
   personMovieCredits: TPersonMediaCredits<TMovie>;
   personTVShowCredits: TPersonMediaCredits<TTVShow>;
+  personLink: PersonLink;
 }) => {
   const { name, homepage, profile_path } = personDetails;
   return (
     <div className="flex flex-col md:flex-row gap-7 md:gap-10 md:mx-10 mt-20 mx-5 ">
       <ProfileCard
-        links={{
-          imdb_id: "nm2858875",
-          facebook_id: "",
-          instagram_id: "sydney_sweeney",
-          twitter_id: "sydney_sweeney",
-          youtube_id: "",
-        }}
+        links={personLink}
         homepage={homepage}
         name={name}
         profilePath={profile_path}
