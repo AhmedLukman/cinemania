64057f13268aea4002b6ac6e1f9479cc805384fe
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-06-12 17:46:12 +0300

    feat: Add usePagination hook for paginating credits

---
+++ src/hooks/usePagination.tsx
@@ -0,0 +1,20 @@
+import { TMovie, TTVShow } from "@/lib/types";
+import { useEffect, useState } from "react";
+
+export const usePagination = ({credits, itemsPerPage}: {
+    credits: TMovie[] | TTVShow[];
+    itemsPerPage: number;
+}) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pagedItems, setPagedItems] = useState(credits);
+
+  useEffect(() => {
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    setPagedItems(credits.slice(startIndex, endIndex));
+  }, [currentPage, credits, itemsPerPage]);
+
+  const totalPages = Math.ceil(credits.length / itemsPerPage);
+
+  return { pagedItems, currentPage, setCurrentPage, totalPages };
+};
