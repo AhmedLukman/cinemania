6087b8faab8d4dd8933472dc6089b8deb6a6a537
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-06-14 18:35:35 +0300

    refactor: Update SuggestionMoviePage to use SuggestionMovieFetchPage for fetching movies

--- src/app/(main)/movie/[id]/[suggestion]/page.tsx
+++ src/app/(main)/movie/[id]/[suggestion]/page.tsx
@@ -1,3 +1,4 @@
+import SuggestionMoviePage from "@/components/page/SuggestionMoviePage";
 import MediaGrid from "@/components/ui/MediaGrid";
 import { MoviesUrl } from "@/lib/constants";
 import { TMediaResponse, TMovie, TMovieDetailsResponse } from "@/lib/types";
@@ -26,20 +27,14 @@ export const generateMetadata = async ({
   };
 };
 
-const SuggestionMoviePage = async ({
+const SuggestionMovieFetchPage = async ({
   params: { id, suggestion },
 }: {
   params: { id: string; suggestion: string };
 }) => {
-  let movies;
-  if (suggestion === "recommendations")
-    movies = (await getMedia(
-      MoviesUrl.Origin + id + "/recommendations?language=en-US&page=1"
-    )) as TMediaResponse<TMovie>;
-  else if (suggestion === "similar")
-    movies = (await getMedia(
-      MoviesUrl.Origin + id + "/similar?language=en-US&page=1"
-    )) as TMediaResponse<TMovie>;
+  const movies = (await getMedia(
+    `${MoviesUrl.Origin}${id}/${suggestion}?language=en-US&page=1`
+  )) as TMediaResponse<TMovie>;
 
   if (!movies || movies.results.length === 0) notFound();
 
@@ -47,19 +42,14 @@ const SuggestionMoviePage = async ({
     MoviesUrl.Origin + id
   )) as TMovie;
 
-  const heading =
-    suggestion === "recommendations"
-      ? "Recommended movies to"
-      : "Similar movies to";
-
   return (
-    <div className="md:p-14 pt-10">
-      <h1 className="text-3xl font-serif text-white my-10 mx-2 md:mx-4 font-bold">
-        {heading} {movieName}
-      </h1>
-      <MediaGrid path="movie" media={movies.results} />;
-    </div>
+    <SuggestionMoviePage
+      movieName={movieName}
+      movies={movies}
+      id={id}
+      suggestion={suggestion}
+    />
   );
 };
 
-export default SuggestionMoviePage;
+export default SuggestionMovieFetchPage;
---
+++ src/components/page/SuggestionMoviePage.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { TMediaResponse, TMovie } from "@/lib/types";
+import React from "react";
+import MediaGrid from "../ui/MediaGrid";
+import PaginationScrollUI from "../ui/PaginationScrollUI";
+import { CircularProgress } from "@nextui-org/react";
+import useServerSidePagination from "@/hooks/useServerSidePagination";
+
+const SuggestionMoviePage = ({
+  movies,
+  movieName,
+  id,
+  suggestion,
+}: {
+  movies: TMediaResponse<TMovie>;
+  movieName: string;
+  id: string;
+  suggestion: string;
+}) => {
+  const {
+    currentPage,
+    isLoading,
+    setCurrentPage,
+    updatedMedia: updatedMovies,
+  } = useServerSidePagination({
+    media: movies.results,
+    id,
+    suggestion,
+  });
+
+  const heading =
+    suggestion === "recommendations"
+      ? "Recommended movies to"
+      : "Similar movies to";
+
+  return (
+    <div className="md:p-14 pt-10">
+      <h1 className="text-3xl font-serif text-white my-10 mx-2 md:mx-4 font-bold">
+        {heading} {movieName}
+      </h1>
+      {isLoading && (
+        <div className="h-[65svh] flex justify-center items-center">
+          <CircularProgress
+            color="secondary"
+            size="lg"
+            aria-label="Loading..."
+          />
+        </div>
+      )}
+      {!isLoading && <MediaGrid path="movie" media={updatedMovies} />}
+      <div className="flex mt-10 justify-center items-center">
+        <PaginationScrollUI
+          currentPage={currentPage}
+          setCurrentPage={setCurrentPage}
+          totalPages={movies.total_pages}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default SuggestionMoviePage;
