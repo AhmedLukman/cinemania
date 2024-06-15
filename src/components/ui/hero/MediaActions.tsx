98cd66bf8a47f3eeb90a91b0a06e33f19f13d5fa
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-06-15 20:04:40 +0300

    refactor: Update MediaActions component to use ViewVideos and FavButton components

--- src/components/ui/hero/MediaActions.tsx
+++ src/components/ui/hero/MediaActions.tsx
@@ -1,22 +1,15 @@
-"use client";
-
+import React from "react";
 import {
-  faHeart as faFilledHeart,
-  faImage,
-} from "@fortawesome/free-solid-svg-icons";
-import { faPlay } from "@fortawesome/free-solid-svg-icons/faPlay";
-import { faHeart } from "@fortawesome/free-regular-svg-icons";
-import { faCircleInfo } from "@fortawesome/free-solid-svg-icons/faCircleInfo";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Button, Link, useDisclosure } from "@nextui-org/react";
-import React, { useState } from "react";
-import VideoModal from "./VideoModal";
-import { usePathname } from "next/navigation";
-import { toast } from "sonner";
-import { useSession } from "next-auth/react";
-import { TMovie, TMovieDetailsResponse, TSingleTVSeasonResponse, TTVShow, TTVShowDetailsResponse } from "@/lib/types";
+  TMovie,
+  TMovieDetailsResponse,
+  TSingleTVSeasonResponse,
+  TTVShow,
+  TTVShowDetailsResponse,
+} from "@/lib/types";
 import ViewImages from "../ViewImages";
 import { getMediaTitle } from "@/lib/utils";
+import ViewVideos from "../ViewVideos";
+import FavButton from "./FavButton";
 
 const MediaActions = ({
   media,
@@ -28,71 +21,16 @@ const MediaActions = ({
     | TTVShow
     | (TSingleTVSeasonResponse & { season: string });
 }) => {
-  const [isIconClicked, setIsIconClicked] = useState(false);
-
-  const { data, status, update } = useSession();
-  const user = data?.user;
-  const isLoading = status === "loading";
-
-  const {
-    isOpen: isVideoOpen,
-    onOpen: onVideoOpen,
-    onOpenChange: onVideoOpenChange,
-  } = useDisclosure();
-
-  const pathname = usePathname();
-
-  const handleFavClick = () => {
-    if (!user) {
-      toast.info("Sign in in order to favorite films");
-      return;
-    }
-    setIsIconClicked((prevState) => !prevState);
-  };
-
   const title = getMediaTitle(media);
-
   return (
     <div className="mt-10 flex items-center justify-between gap-2 md:justify-start md:gap-5">
-      <VideoModal
-        mediaId={media.id}
-        title={title}
-        isOpen={isVideoOpen}
-        onOpenChange={onVideoOpenChange}
-      />
-      <Button
-        onPress={onVideoOpen}
-        endContent={<FontAwesomeIcon icon={faPlay} />}
-      >
-        Watch Clips
-      </Button>
+      {"videos" in media && (
+        <ViewVideos videoData={media.videos.results} title={title} />
+      )}
       {"images" in media && (
         <ViewImages imageData={media.images} title={title} />
       )}
-      {(pathname === "/tv" || pathname === "/movie") && (
-        <Button
-          as={Link}
-          href={`${pathname}/${media.id}`}
-          variant="bordered"
-          className="text-white"
-          endContent={<FontAwesomeIcon icon={faCircleInfo} />}
-        >
-          More details
-        </Button>
-      )}
-      <Button
-        variant="light"
-        onPress={handleFavClick}
-        className="text-white"
-        startContent={
-          <FontAwesomeIcon
-            icon={isIconClicked ? faFilledHeart : faHeart}
-            size="xl"
-          />
-        }
-      >
-        120
-      </Button>
+      <FavButton />
     </div>
   );
 };
