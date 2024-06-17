2534a51f8f1f629d983419165364dd9c67c649be
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-06-17 21:14:59 +0300

    style: Update loading component height

--- src/app/(main)/movie/loading.tsx
+++ src/app/(main)/movie/loading.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const Loading = () => {
   return (
-    <div className="fixed inset-0 flex z-50 justify-center items-center">
+    <div className="h-[85vh] flex z-50 justify-center items-center">
       <CircularProgress color="secondary" size="lg" aria-label="Loading..." />
     </div>
   );
